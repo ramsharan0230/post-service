65c31212c4eec2ef27ddeed803302993af887489
ram <ramsharan0230@gmail.com>
2025-11-16 23:55:35 +0000

    .env file created

--- src/main/java/com/video/processing/ProcessingApplication.java
+++ src/main/java/com/video/processing/ProcessingApplication.java
@@ -4,12 +4,21 @@ import java.util.Random;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
+@EnableAsync
 public class ProcessingApplication {
 	public static void main(String[] args) {
-		Random random = new Random();
-		System.out.println("random int: "+random.nextInt(100000));
+		// Load .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        // Export to system properties so Spring can use them
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
 		SpringApplication.run(ProcessingApplication.class, args);
 	}
 
